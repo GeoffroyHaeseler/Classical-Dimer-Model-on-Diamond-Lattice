5644e3e7175925dd81d083b2fa02118d678d84fc
GeoffroyHaeseler <geoffroy.haeseler@gmail.com>
2026-06-08 16:21:07 +0200

    Delete growTheWorm.h

--- growTheWorm.h
+++
@@ -1,6 +0,0 @@
-/// <summary>
-/// Choose the next tetrahedron. If it is a worm piece, return false : we need to close the loop. If not, return true
-/// </summary>
-/// <param name="Worm1s"></param>
-/// <returns></returns>
-void growTheWorm(int* x, int* y, int* z, char* tetra, int* n);

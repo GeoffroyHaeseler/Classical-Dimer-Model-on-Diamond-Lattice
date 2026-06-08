774690b8e1aa6dd48275e65d90dabf19815ef648
GeoffroyHaeseler <geoffroy.haeseler@gmail.com>
2026-06-08 16:21:01 +0200

    Delete initialise.h

--- initialise.h
+++
@@ -1,5 +0,0 @@
-void initialise();
-void magFieldInitialisation(double theta, double phi);
-void latticeInitialisation();
-void differenceInitialisation();
-bool probaInitialisation(double kT, double theta, double phi);

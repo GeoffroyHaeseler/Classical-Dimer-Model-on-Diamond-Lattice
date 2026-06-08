c4e6edfe135d25ac781f28877d3f841b1fe60e71
GeoffroyHaeseler <geoffroy.haeseler@gmail.com>
2026-06-08 16:20:00 +0200

    Delete spin.h

--- spin.h
+++
@@ -1,3 +0,0 @@
-void spinCoordinates(char spin_tetra, int spin_n, double* spin_coord_x, double* spin_coord_y, double* spin_coord_z);
-int spinLine(int ix, int iy, int iz, char tetrahedron, int submit);
-int pseudospin(int ix, int iy, int iz, int itetra, int isubmit);
